7e5f80eec941d7d5f6a229e81473000bc8b99a41
Aditya <aditya.chaudhary1558@gmail.com>
2024-01-11 23:40:40 +0530

    made the logout button with function and restricted route for non logged user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,28 @@
+'use client'
+
 import React from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import toast from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
 
 const Profile = () => {
+
+  const router = useRouter();
+
+
+  const logout = async() => {
+      try{
+        await axios.get("/api/users/logout");
+        toast.success("Logout Successfull")
+      }catch(err: any){
+        console.log(err.message);
+        toast.error(err.message);
+      }
+  }
+
   return (
-    <div>Profile</div>
+    <button className='bg-blue-500 mt-4 hover:bg-blue-700 text-white py-2 px-4 rounded' onClick={logout}>Logout</button>
   )
 }
 
